c9515ea33707b91597dc3700f7b9f6ac7f179084
Myron Holmes <1dequanh@gmail.com>
2023-05-11 18:27:22 -0400

    Get users name

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,9 +1,12 @@
+import java.util.Optional;
 import java.util.Scanner;
 
 public class GuessingGame {
     public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Hello player, what's your name?");
-
+        System.out.println("(Enter your name)");
+        String userName = input.nextLine();
     }
 }
