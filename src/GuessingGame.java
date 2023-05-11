1a8fdcebc939ef243180c4bad9a6174983b383ce
Myron Holmes <1dequanh@gmail.com>
2023-05-11 13:39:40 -0400

    First commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main (String[] args){
+
+        System.out.println("Hello player, what's your name?");
+
+    }
+}
---
+++ src/Mars.java
@@ -0,0 +1,40 @@
+public class Mars {
+    public static void main (String[] args) throws InterruptedException {
+        String colonyName = "Marzone";
+        int shipPopulation = 300;
+        double meals = 4_000.00;
+        boolean landing = landingCheck(40);
+
+        meals -= (shipPopulation * 0.75);
+        meals -= (shipPopulation * 0.75);
+        System.out.println(meals);
+
+        meals += meals * .50;
+        shipPopulation += 5;
+
+        String landingLocation = "The Ocean";
+        if (landingLocation.equalsIgnoreCase("The Plain")){
+            System.out.println("Bbzzz Landing on the Plain");
+        }else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
+        }
+
+    }
+
+    public static boolean landingCheck(int minutesLeft) throws InterruptedException {
+        for (int minutes= 0; minutes <= minutesLeft; minutes++){
+            if ((minutes % 2 == 0) && (minutes % 3 == 0)){
+                System.out.println("Keep Center");
+            }else if (minutes % 2 == 0){
+                System.out.println("Right");
+            }else if (minutes % 3 == 0){
+                System.out.println("left");
+            }else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+        return false;
+    }
+}
